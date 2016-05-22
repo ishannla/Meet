edd067481853dc86147ce1d93a39f486b4a5d181
Ishan Narula <ishan.narula@gmail.com>
2016-05-22 00:35:58 -0700

    Added blank nearby/map tabs

---
+++ app/src/main/java/com/sabersoft/ishannarula/meet/NearbyFragment.java
@@ -0,0 +1,19 @@
+package com.sabersoft.ishannarula.meet;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class NearbyFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_nearby, container, false);
+    }
+
+}
