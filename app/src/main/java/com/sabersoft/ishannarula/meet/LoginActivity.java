d40ddbf538d832cf588efcef88471ee3c97ac803
Ishan Narula <ishan.narula@gmail.com>
2016-05-22 03:00:29 -0700

    User authentication and signup functional

--- app/src/main/java/com/sabersoft/ishannarula/meet/LoginActivity.java
+++ app/src/main/java/com/sabersoft/ishannarula/meet/LoginActivity.java
@@ -1,11 +1,20 @@
 package com.sabersoft.ishannarula.meet;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.firebase.client.Firebase;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -13,27 +22,50 @@ public class LoginActivity extends AppCompatActivity {
     EditText passwordField;
     Button submitButton;
     Button signupButton;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        Firebase.setAndroidContext(this);
+        Firebase firebase = new Firebase("https://meet-2332f.firebaseio.com/");
+        final FirebaseAuth auth = FirebaseAuth.getInstance();
+
         emailField = (EditText)findViewById(R.id.emailField);
         passwordField = (EditText)findViewById(R.id.passwordField);
         submitButton = (Button) findViewById(R.id.submitButton);
         signupButton = (Button) findViewById(R.id.signupButton);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
         final Intent mainIntent = new Intent(this, MainActivity.class);
         final Intent signupIntent = new Intent(this, SignupActivity.class);
 
+        progressBar.setVisibility(View.INVISIBLE);
+
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                progressBar.setVisibility(View.VISIBLE);
+
                 String email = emailField.getText().toString();
                 String password = passwordField.getText().toString();
 
-                startActivity(mainIntent);
+                auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressBar.setVisibility(View.INVISIBLE);
+
+                        if (task.isSuccessful())
+                            startActivity(mainIntent);
+
+                        else
+                            Toast.makeText(getApplicationContext(), "Your email/password is incorrect", Toast.LENGTH_LONG).show();
+
+                    }
+                });
             }
         });
 
--- app/src/main/java/com/sabersoft/ishannarula/meet/SignupActivity.java
+++ app/src/main/java/com/sabersoft/ishannarula/meet/SignupActivity.java
@@ -1,39 +1,80 @@
 package com.sabersoft.ishannarula.meet;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.firebase.client.Firebase;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.sabersoft.ishannarula.meet.objects.User;
 
 public class SignupActivity extends AppCompatActivity {
 
     EditText firstnameField;
     EditText lastnameField;
+    EditText phoneField;
     EditText emailField;
     EditText passwordField;
     Button submitButton;
+    ProgressBar progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        
+        Firebase.setAndroidContext(this);
+        final Firebase users = new Firebase("https://meet-2332f.firebaseio.com/users");
+        final FirebaseAuth auth = FirebaseAuth.getInstance();
 
         firstnameField = (EditText)findViewById(R.id.firstnameField);
         lastnameField = (EditText)findViewById(R.id.lastnameField);
+        phoneField = (EditText)findViewById(R.id.phoneField);
         emailField = (EditText)findViewById(R.id.emailField);
-        passwordField = (EditText)findViewById(R.id.passwordField);
+        passwordField = (EditText) findViewById(R.id.passwordField);
         submitButton = (Button) findViewById(R.id.submitButton);
+        progress = (ProgressBar) findViewById(R.id.progressBar);
+
+        progress.setVisibility(View.INVISIBLE);
 
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String firstName = firstnameField.getText().toString();
-                String lastName = lastnameField.getText().toString();
-                String email = emailField.getText().toString();
-                String password = passwordField.getText().toString();
+
+                progress.setVisibility(View.VISIBLE);
+
+                final String firstName = firstnameField.getText().toString();
+                final String lastName = lastnameField.getText().toString();
+                final String phoneNumber = phoneField.getText().toString();
+                final String email = emailField.getText().toString();
+                final String password = passwordField.getText().toString();
+
+
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        progress.setVisibility(View.INVISIBLE);
+
+                        if (task.isSuccessful()){
+                            Toast.makeText(getApplicationContext(), "Account created!", Toast.LENGTH_LONG).show();
+                            User newUser = new User(firstName, lastName, phoneNumber, email);
+                            users.push().setValue(newUser);
+                        }
+
+                        else
+                            Toast.makeText(getApplicationContext(), "An error has occured.", Toast.LENGTH_LONG).show();
+                    }
+                });
+
 
             }
         });
