26a07b0db2c498762eace77d0d8f46ed0797168c
Ishan Narula <ishan.narula@gmail.com>
2016-05-22 02:33:56 -0700

    Added relevant Firebase/Google dependencies

--- app/src/main/java/com/sabersoft/ishannarula/meet/SignupActivity.java
+++ app/src/main/java/com/sabersoft/ishannarula/meet/SignupActivity.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.firebase.client.Firebase;
+
 public class SignupActivity extends AppCompatActivity {
 
     EditText firstnameField;
